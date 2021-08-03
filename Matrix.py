8eb6cd4a77a31b63bcf26d1b8ff75ae7bef3efb7
Siddharth Singh Rana <siddharth_sr@cs.iitr.ac.in>
2021-08-03 22:23:47 +0530

    add exceptions when invalid matrix orders given

--- Matrix.py
+++ Matrix.py
@@ -22,57 +22,86 @@ import unittest
 #         break
 #
 # print("\n")
+class Error(Exception):
+    """ Base class for other exceptions """
+    pass
+
+class NotList(Error):
+    """ Raised when the input passed is not a list """
+    pass
 
 class Matrix:
 
     def __init__(self, List):
-        self.List = List
+        try:
+            if type(List) == list:
+                self.List = List
+            else:
+                raise NotList
+        except NotList:
+            print("List not passed, please pass a nested list")
+
 
     def display(self):
         for i in self.List:
             print(i)
 
     def __add__(self, other):
-        Temp = Matrix([])
-        Temp.List = [[0 for i in range(len(self.List[0]))] for j in range(len(self.List))]
-        for i in range(len(self.List)):
-            for j in range(len(self.List[0])):
-                Temp.List[i][j] = self.List[i][j] + other.List[i][j]
-        return Temp
+        if len(self.List) == len(other.List) and len(self.List[0]) == len(other.List[0]):
+            temp = Matrix([])
+            temp.List = [[0 for i in range(len(self.List[0]))] for j in range(len(self.List))]
+            for i in range(len(self.List)):
+                for j in range(len(self.List[0])):
+                    temp.List[i][j] = self.List[i][j] + other.List[i][j]
+            return temp
+        else:
+            raise Exception("Matrices' order is different, cannot add")
 
     def __sub__(self, other):
-        Temp = Matrix([])
-        Temp.List = [[0 for i in range(len(self.List[0]))] for j in range(len(self.List))]
-        for i in range(len(self.List)):
-            for j in range(len(self.List[0])):
-                Temp.List[i][j] = self.List[i][j] - other.List[i][j]
-        return Temp
+        if len(self.List) == len(other.List) and len(self.List[0]) == len(other.List[0]):
+            temp = Matrix([])
+            temp.List = [[0 for i in range(len(self.List[0]))] for j in range(len(self.List))]
+            for i in range(len(self.List)):
+                for j in range(len(self.List[0])):
+                    temp.List[i][j] = self.List[i][j] - other.List[i][j]
+            return temp
+        else:
+            raise Exception("Matrices' order is different, cannot subtract")
 
     def __mul__(self, other):
-        result = Matrix([])
-        result.List = [[0 for i in range(len(other.List[0]))] for j in range(len(self.List))]
-        for i in range(len(self.List)):
-            for j in range(len(other.List[0])):
-                for k in range(len(other.List)):
-                    result.List[i][j] += self.List[i][k] * other.List[k][j]
-        return result
+        if len(self.List[0]) == len(other.List):
+            result = Matrix([])
+            result.List = [[0 for i in range(len(other.List[0]))] for j in range(len(self.List))]
+            for i in range(len(self.List)):
+                for j in range(len(other.List[0])):
+                    for k in range(len(other.List)):
+                        result.List[i][j] += self.List[i][k] * other.List[k][j]
+            return result
+        else:
+            raise Exception("columns in self not same as rows in other, cannot multiply")
 
     def __pow__(self, power, modulo=None):
-        Temp = Matrix([])
-        Temp.List = self.List
-        for i in range(power-1):
-            Temp = Temp * self
-        return Temp
+        if len(self.List) == len(self.List[0]):
+            temp = Matrix([])
+            temp.List = self.List
+            for i in range(power-1):
+                temp = temp * self
+            return temp
+        else:
+            raise Exception("Not a square matrix, cannot exponentiate")
 
     def getMatrixDeterminant(self, m):
-        if len(m) == 2:
-            return m[0][0] * m[1][1] - m[0][1] * m[1][0]
-        determinant = 0
-        for c in range(len(m)):
-            metric = [row[:c] + row[c + 1:] for row in (m[:0] + m[1:])]
-            obj = Matrix(metric)
-            determinant += ((-1) ** c) * m[0][c] * obj.getMatrixDeterminant(metric)
-        return determinant
+        if len(self.List) == len(self.List[0]):
+            if len(m) == 2:
+                return m[0][0] * m[1][1] - m[0][1] * m[1][0]
+            determinant = 0
+            for c in range(len(m)):
+                metric = [row[:c] + row[c + 1:] for row in (m[:0] + m[1:])]
+                obj = Matrix(metric)
+                determinant += ((-1) ** c) * m[0][c] * obj.getMatrixDeterminant(metric)
+            return determinant
+        else:
+            raise Exception("Not a square matrix, cannot find determinant")
 
 
 # Matrix1 = Matrix(matrix1)
